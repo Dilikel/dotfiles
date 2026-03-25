8892aa95c0c25696043c52743115b17eb9b4df39
Dilikel <shiryayev_vova@bk.ru>
2026-03-25 12:35:29 +0300

    fix nvim

--- base/.config/nvim/lua/plugins/snacks.lua
+++ base/.config/nvim/lua/plugins/snacks.lua
@@ -3,8 +3,8 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
-        hidden = true, -- для скрытых файлов
-        ignored = true, -- для файлов .gitignore
+        hidden = true,
+        ignored = true,
       },
     },
   },
